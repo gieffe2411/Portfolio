d85a31c1925c7efee575e8c345e6b9e68a3cc55e
GIF2411 <nitritoantico@gmail.com>
2026-06-19 18:32:51 +0200

    mamma mia tutto the site: css, index, friends, public e professional

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-test
+import flask
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html", title="Copertina", page="home")
+
+@app.route("/friends")
+def friends():
+    return render_template("friends.html", title="Amici", page="friends")
+
+@app.route("/professional")
+def professional():
+    return render_template("professional.html", title="Professionale", page="professional")
+
+@app.route("/public")
+def public():
+    return render_template("public.html", title="Pubblico", page="public")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
