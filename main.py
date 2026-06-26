f8e981117d630f65255929b30eaf46d25f6ed7f3
GIF2411 <nitritoantico@gmail.com>
2026-06-26 18:48:50 +0200

    added gitignore, updated flask to get discord status. updated public template

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 import flask
 from flask import Flask, render_template
+import requests
 app = Flask(__name__)
 
+
+USER_ID = "972113138612854824"
+
+def get_discord_data():
+    try:
+        res = requests.get(f"https://api.lanyard.rest/v1/users/{USER_ID}", timeout=3)
+        data = res.json()
+
+        if not data["success"]:
+            return None
+
+        d = data["data"]
+
+        activity = None
+        if d["activities"]:
+            act = d["activities"][0]
+            activity = act.get("state")
+
+        return {
+            "username": d["discord_user"].get("display_name") or d["discord_user"]["username"],
+            "avatar": f"https://cdn.discordapp.com/avatars/{d['discord_user']['id']}/{d['discord_user']['avatar']}.png",
+            "status": d["discord_status"],
+            "activity": activity,
+            "emoji": d["activities"][0]["emoji"]["name"]
+        }
+
+    except:
+        return None
 
 @app.route("/")
 def home():
@@ -17,7 +46,7 @@ def professional():
 
 @app.route("/public")
 def public():
-    return render_template("public.html", title="Pubblico", page="public")
+    return render_template("public.html", title="Pubblico", page="public", discord=get_discord_data())
 
 
 if __name__ == "__main__":
